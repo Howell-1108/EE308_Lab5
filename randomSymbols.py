b6593da8a90ee06f9a4eb48387dff4cac841943b
Howell <1102018054@qq.com>
2021-10-18 18:02:10 +0800

    function finish

--- randomSymbols.py
+++ randomSymbols.py
@@ -1,5 +1,6 @@
 import random
-def random_symbols(grade):
+def random_symbols():
+    # Four operation symbols
     operation = ['+', '-', '*', '/'] 
     if grade < 3:
         randSym = operation[random.randint(0, 1)]
