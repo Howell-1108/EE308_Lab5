587765ea4dfd534a21b87ab68d41a64845bb3921
Jodie Lin <90860297+JodieLin@users.noreply.github.com>
2021-10-18 17:24:40 +0800

    Result 更新

---
+++ Result.py
@@ -0,0 +1,7 @@
+def DeResult( RealResult, UserResult ):
+    #Check whether the real result which is calculated by computer is the same with that of users or not.
+    if RealResult == UserResult:
+        return 1
+    else:
+        return 0
+
